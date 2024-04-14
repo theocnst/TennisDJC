08dac283dcad9740f935a270449c2d0a82f03873
AlexAntohi <115532036+AlexAntohi@users.noreply.github.com>
2024-04-14 23:54:19 +0300

    Game finished

--- TennisUnityApp/Assets/Scripts/ShotManager.cs
+++ TennisUnityApp/Assets/Scripts/ShotManager.cs
@@ -12,11 +12,12 @@ public class Shot
     }
 }
 
+[SerializeField]
 public class ShotManager : MonoBehaviour
 {
-    public Shot flat = new Shot(5, 8);
-    public Shot topSpin = new Shot(3, 15);
-    public Shot flatServe = new Shot(5, 8);
-    public Shot topSpinServe = new Shot(3, 15);
-    public Shot defaultHit = new Shot(7, 8);
+    public Shot flat = new Shot(8, 11);
+    public Shot topSpin = new Shot(3.5f, 15);
+    public Shot flatServe = new Shot(8, 11);
+    public Shot topSpinServe = new Shot(3.5f, 15);
+    public Shot defaultHit = new Shot(10, 10);
 }

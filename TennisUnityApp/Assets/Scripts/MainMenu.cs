f9e3356c1f730c979f2a6fbd13c60b0cc80fbaee
Sm0k3Official <cosmyn_dobre@yahoo.com>
2024-04-14 10:14:14 +0300

    Added menu scene. Added menu functionalities. Added menu animations for buttons.

---
+++ TennisUnityApp/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject mainMenuPanel;
+
+    [SerializeField]
+    private GameObject settingsPanel;
+
+    [SerializeField]
+    private Button settingsButton;
+
+    [SerializeField]
+    private Button backButton;
+
+    public void Start()
+    {
+        mainMenuPanel.SetActive(true);
+        settingsPanel.SetActive(false);
+    }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void OpenSettings()
+    {
+        settingsButton.animator.Rebind();
+
+        mainMenuPanel.SetActive(false);
+        settingsPanel.SetActive(true);
+    }
+
+    public void CloseSettings()
+    {
+        backButton.animator.Rebind();
+
+        mainMenuPanel.SetActive(true);
+        settingsPanel.SetActive(false);
+    }
+}
